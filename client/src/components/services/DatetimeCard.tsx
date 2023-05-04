3b634d47e0290b2e59d98fa71fbf6cec89d33cb5
Foster <fosterunlimited0123@gmail.com>
2023-05-04 09:48:44 +0530

    Exertrack(welcome_page_user_service_card_in_progress)

--- client/src/components/services/DatetimeCard.tsx
+++ client/src/components/services/DatetimeCard.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "preact/hooks";
+
 const DatetimeCard = () => {
     const now = new Date();
+    const [ time, setTime ] = useState(now)
+    
+    setInterval(() => setTime(now), 1000)
     return (
         <div className="service-card">
             <div className="datetime-card-container">
-                <header>{now.toLocaleDateString()}</header>
+                <header>{time.toLocaleTimeString()}</header>
             </div>
         </div>
     )
